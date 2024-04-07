ee5715abc05e3bfa18b8b847c97e9d25b1516112
engageintellect <jc9361@gmail.com>
2024-04-07 01:16:48 -0700

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata = {
     ? new URL(`https://${process.env.VERCEL_URL}`)
     : undefined,
   title: {
-    default: 'Next.js AI Chatbot',
+    default: 'vercel-ai-sdk-3',
     template: `%s - Next.js AI Chatbot`
   },
   description: 'An AI-powered chatbot template built with Next.js and Vercel.',
